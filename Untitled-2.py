7187902f5cedaa96e8efd49bffa917dc615e4368
anuragrao04 <raoanu2004@gmail.com>
2021-02-01 13:33:07 +0530

    renamed and added a few python examples

---
+++ Untitled-2.py
@@ -0,0 +1,34 @@
+def traffic_light(c):
+    c = c.lower()
+    
+    if c not in {'green','yellow','red'}:
+        print("Invalid input, try again")
+        return 3
+
+    elif c == 'red':
+        return 0
+
+    elif c == 'yellow':
+        return 1
+
+    else:
+        return 2
+
+
+def light(n):
+    if n == 0:
+        print("STOP! Your life is precious")
+
+    elif n == 1:
+        print("please WAIT till the light is green")
+
+    elif n == 2:
+        print("GO! Thank you for being patient")
+  
+        
+
+
+
+while True:
+    color = input("Enter light: (green/yellow/red): ")
+    light(traffic_light(color))
