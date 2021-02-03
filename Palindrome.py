723eb74b0f65958e7fc33a4aa41ca241135b2dc2
anuragrao04 <raoanu2004@gmail.com>
2021-02-03 14:04:28 +0530

    improved readability

--- Palindrome.py
+++ Palindrome.py
@@ -1,19 +1,19 @@
 #Code to check weather a given word or sentence is a palindrome
 while True: 
-    word = input("Enter the word to check: ")
+    word = input("Enter the word to check: ").lower()
     new = ""
     word_length = len(word)
     word_length = word_length - 1
     
     
     for i in range(word_length, -1, -1):
-        new = new + word[i].lower()
-        print(word[i].lower())
+        new = new + word[i]
+        print(word[i])
 
     
 
 
-    if new == word.lower():
+    if new == word:
         print(f"{word} is a palindrome")
 
     else:
